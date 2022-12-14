8a598741d34953cb9cd3a4db267d89377a7e5aec
Lucas Deodato Silva <lucas.1657025@discente.uemg.br>
2022-12-14 13:41:37 -0300

    first commit

---
+++ br1/app.js
@@ -0,0 +1,14 @@
+function changecollor(){
+    var a = document.getElementById('mouse')
+    a.style.backgroundColor = 'blue'
+    a.display = 'inline-block'  
+    a.padding = '10px'
+    a.style.backgroundColor = 'red'  
+}
+
+function deschangecollor(){
+    var a = document.getElementById('mouse')
+    a.style.backgroundColor = 'blue'
+    a.display = 'inline-block'  
+    a.padding = '10px'
+}
